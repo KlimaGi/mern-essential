206b97afa505c52b229d68b79a9d6c58a596d305
Giedre <giedre.klima@gmail.com>
2021-11-30 13:00:46 +0200

    get enpoints

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyparser from "body-parser";
+import cors from "cors";
+import routes from "./routes/soccerRoutes";
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // mongo connection
 mongoose.Promise = global.Promise;
@@ -15,6 +17,11 @@ mongoose.connect("mongodb://localhost/soccerDB", {
 // bodyparser setup
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
+
+// CORS setup
+app.use(cors());
+
+routes(app);
 
 app.get("/", (req, res) => {
   res.send(`Our Soccer application is running on port: ${PORT}`);
